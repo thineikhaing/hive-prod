c4ec0ab10c1f9b5cf63cdc2cc04eead4a1020bf1
Khine <khinephyuphyu@rs-v.com>
2014-05-30 10:10:08 +0800

    set url configuration for email template depends on the enviroment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,13 +4,20 @@ class UserMailer < ActionMailer::Base
   def account_verification(username, email, verification_code)
     @verify = verification_code
     @username = username
-    @email = email
-
+    if Rails.env.development? || Rails.env.testing?
+      @email = "http://localhost:5000/verify?email=" + email
+    elsif Rails.env.staging? || Rails.env.production?
+      @email = "http://h1ve-staging.herokuapp.com/verify?email=" + email
+    end
     mail(:to => email, :subject => "Please complete your verification")
   end
 
   def password_reset(user)
-    @user = user
+    if Rails.env.development? || Rails.env.testing?
+      @reset_pwd_url = "http://localhost:5000/reset_password?token=" + user.reset_password_token
+    elsif Rails.env.staging? || Rails.env.production?
+      @reset_pwd_url = "http://h1ve-staging.herokuapp.com/reset_password?token=" + user.reset_password_token
+    end
     mail :to => user.email, :subject => "Password Reset"
   end
 
