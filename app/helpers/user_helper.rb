e33303426e2d46019658f14dfee166f4af580019
Khine <khinephyuphyu@rs-v.com>
2014-06-13 10:30:43 +0800

    - use secure method to check current user using devise token and user-id

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,5 +1,16 @@
 module UserHelper
   def current_user
-    Devuser.find_by_id(session[:session_devuser_id])
+    if params[:auth_token].present? && params[:user_id].present?
+      p "if"
+      user = User.find(params[:user_id])  #get user by user_id
+      if user.present?
+        if Devise.secure_compare(user.authentication_token, params[:auth_token]) == true #check the user auth_token and params are the same
+          user
+        end
+      end
+    elsif  session[:session_devuser_id].present?
+      p "else"
+      Devuser.find_by_id(session[:session_devuser_id])
+    end
   end
 end
