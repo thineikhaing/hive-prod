91cc8e1a2a33a263c085d222a625610492bb76ee
Khine <khinephyuphyu@rs-v.com>
2014-06-13 16:07:25 +0800

    use the helper to check current user

--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -4,22 +4,23 @@ class Api::TopicsController < ApplicationController
       hiveApplication = HiveApplication.find_by_api_key(params[:app_key])
 
       if hiveApplication.present?
-        user = User.find_by_authentication_token (params[:auth_token]) if params[:auth_token].present?
+        #user = User.find_by_authentication_token (params[:auth_token]) if params[:auth_token].present?
 
-        if user.present?
-          topic = Topic.create(title: params[:title], user_id: user.id, topic_sub_type: params[:topic_sub_type], hiveapplication_id: hiveApplication.id, place_id: params[:place_id])
+        if current_user.present?
+
+          topic = Topic.create(title: params[:title], user_id: current_user.id, topic_sub_type: params[:topic_sub_type], hiveapplication_id: hiveApplication.id, place_id: params[:place_id])
 
           render json: { topic: topic }
         else
-          p 1
+
           render json: { status: false }
         end
       else
-        p 2
+
         render json: { status: false }
       end
     else
-      p 3
+
       render json: { status: false }
     end
   end
