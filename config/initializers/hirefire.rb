6f63ae167f11c5f0d5d163a2dfee24a8b3becabf
Khine <tardis@LeeMinHo.local>
2014-08-11 09:48:17 +0800

    - change hirefire.rb to integrate with rails 4

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,5 +1,5 @@
 HireFire::Resource.configure do |config|
   config.dyno(:dj_worker) do
-    HireFire::Macro::Delayed::Job.queue
+    HireFire::Macro::Delayed::Job.queue(mapper: :active_record)
   end
 end
