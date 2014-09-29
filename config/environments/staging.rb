5ec2a08d6dbd20202b9758d93c3dd1a1db5ab5fd
Khine <tardis@LeeMinHo.local>
2014-09-29 14:56:36 +0800

    set eager load to false for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,6 +4,9 @@ Hive::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # Do not eager load code on boot.
+  config.eager_load = false
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
