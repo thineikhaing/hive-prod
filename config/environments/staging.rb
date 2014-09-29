ebe4999a4f4bcd1ce4b57053276c80919066b930
Khine <tardis@LeeMinHo.local>
2014-09-29 14:37:34 +0800

    change staging app name

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-SocialCalendar::Application.configure do
+Rails.Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
