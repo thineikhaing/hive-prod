3a7633e6a1370c82052530000ce56e43fd8a32a1
thin <kale@rs-v.com>
2015-05-27 17:48:17 +0800

    add the scheduler to flash the data

---
+++ config/initializers/task_scheduler.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'rufus/scheduler'
+scheduler = Rufus::Scheduler.new
+
+#scheduler.every  :monday do # Use any day of the week or :weekend, :weekday
+#  CarActionLog.delete_all
+#end
+scheduler.cron("00 09 * * mon") do
+  # whatever...
+  CarActionLog.delete_all
+end
+
+scheduler.cron("5 0 * * *") do
+  CarActionLog.delete_all
+end
+
+#scheduler.every '3s' do
+#  puts 'Hello... Rufus'
+#end
