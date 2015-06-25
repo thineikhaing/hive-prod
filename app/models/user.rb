073a16ebf94221d06e26a50f26721cd513bb67a4
thin <thin@herenow.io>
2015-06-25 17:17:47 +0800

    update points

--- app/models/user.rb
+++ app/models/user.rb
@@ -184,8 +184,8 @@ class User < ActiveRecord::Base
       a1.last_known_longitude = a2.longitude
       a1.save
 
-      a1.last_known_latitude -= 0.01
-      a1.last_known_longitude -= 0.05
+      a1.last_known_latitude -= 0.003
+      a1.last_known_longitude -= 0.004
 
       a1.save
 
