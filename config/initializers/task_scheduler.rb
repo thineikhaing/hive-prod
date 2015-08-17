f7d038b19bb635873523d37b6ba0544cc057b951
thin <thin@herenow.io>
2015-08-17 15:11:12 +0800

    cron job 9am at mon

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,17 +2,17 @@ require 'rubygems'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.every  :monday do # Use any day of the week or :weekend, :weekday
-  CarActionLog.delete_all
-end
-
-scheduler.cron("0 21 * * mon") do
-  CarActionLog.delete_all
-end
-
-#scheduler.in '7d' do
+#scheduler.every  :monday do # Use any day of the week or :weekend, :weekday
 #  CarActionLog.delete_all
 #end
+
+scheduler.cron "00 09 * * mon" do
+  CarActionLog.delete_all
+end
+
+scheduler.in '7d' do
+  CarActionLog.delete_all
+end
 
 #scheduler.every '3s' do
 #  puts 'Hello... Rufus'
