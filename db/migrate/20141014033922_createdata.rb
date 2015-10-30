f75fb95f7afeacfddc150badd797584727a469a7
thin <thin@herenow.io>
2015-10-30 13:30:55 +0800

    add additional data for topic and post

--- db/migrate/20141014033922_createdata.rb
+++ db/migrate/20141014033922_createdata.rb
@@ -66,8 +66,6 @@ class Createdata < ActiveRecord::Migration
 
     end
 
-
-
     CSV.foreach("db/migrate/20140328083723_stations_seed.csv") do |row|
       Place.create(name: row[0], latitude: row[1], longitude: row[2], address: row[3], locality: row[4], country: row[5], img_url: row[6], user_id: admin.id, source: 0)
       puts "Created: #{row}"
