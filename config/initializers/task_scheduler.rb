cf6c48a880ba51c9fa7b91bdcbb62d04868a6c2a
thin <thin@herenow.io>
2015-11-25 16:36:29 +0800

    update user daily point every day by schedule

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -10,14 +10,19 @@ scheduler.cron "00 09 * * mon" do
   CarActionLog.delete_all
 end
 
-scheduler.in '7d' do
-  CarActionLog.delete_all
+scheduler.cron '5 0 * * *' do
+  users = User.all
+  users.each do |user|
+    user.daily_points += 10
+    user.save
+  end
 end
 
-scheduler.every '3s' do
-  puts 'Hello... Rufus'
-  CarActionLog.delete_all
-end
+#scheduler.every '3s' do
+
+#end
+
+
 #
 #scheduler.every '2s' do
 #  puts 'check blood pressure'
