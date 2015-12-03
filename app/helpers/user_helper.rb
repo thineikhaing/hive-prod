74015f74da654b640701fb8909b88a3e1da2852c
thin <thin@herenow.io>
2015-12-03 10:24:32 +0800

    current user by userid and authentication token

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -7,6 +7,13 @@ module UserHelper
           user
         end
       end
+    elsif params[:auth_token].present?
+      user = User.find_by_authentication_token(params[:auth_token])  #get user by user_id
+      if user.present?
+        if Devise.secure_compare(user.authentication_token, params[:auth_token]) == true #check the user auth_token and params are the same
+          user
+        end
+      end
     elsif  session[:session_devuser_id].present?
       Devuser.find_by_id(session[:session_devuser_id])
     end
