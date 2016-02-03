bbcde80d09e1ff2249197fd0b4653475500d4806
thin <thin@herenow.io>
2016-02-03 09:43:30 +0800

    task scheduler 2min

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -19,10 +19,10 @@ scheduler.cron '05 00 * * *' do
   end
 end
 
-#scheduler.every 2.minutes do
-#  p "check accident!"
-#  SgAccidentHistory.get_incident_and_breakdown
-#end
+scheduler.every 2.minutes do
+  p "check accident!"
+  SgAccidentHistory.get_incident_and_breakdown
+end
 
 
 
