47c0f59694e6dd8d1469630d41cf066de27078c7
thin <thin@herenow.io>
2016-04-08 13:52:30 +0800

    remove rescue in user helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,27 +1,22 @@
 module UserHelper
   def current_user
-    begin
-      if params[:auth_token].present? && params[:user_id].present?
-        user = User.find(params[:user_id])  #get user by user_id
-        if user.nil?
-          if Devise.secure_compare(user.authentication_token, params[:auth_token]) == true #check the user auth_token and params are the same
-            user
-          end
+    if params[:auth_token].present? && params[:user_id].present?
+      user = User.find(params[:user_id])  #get user by user_id
+      if user.present?
+        if Devise.secure_compare(user.authentication_token, params[:auth_token]) == true #check the user auth_token and params are the same
+          user
         end
-      elsif params[:auth_token].present?
-        user = User.find_by_authentication_token(params[:auth_token])  #get user by user_id
-        if user.present?
-          if Devise.secure_compare(user.authentication_token, params[:auth_token]) == true #check the user auth_token and params are the same
-            user
-          end
-        end
-      elsif  session[:session_devuser_id].present?
-        Devuser.find_by_id(session[:session_devuser_id])
       end
-    rescue ActiveRecord::RecordNotFound
-      return
+    elsif params[:auth_token].present?
+      user = User.find_by_authentication_token(params[:auth_token])  #get user by user_id
+      if user.present?
+        if Devise.secure_compare(user.authentication_token, params[:auth_token]) == true #check the user auth_token and params are the same
+          user
+        end
+      end
+    elsif  session[:session_devuser_id].present?
+      Devuser.find_by_id(session[:session_devuser_id])
     end
-
   end
 end
                                                                                                                                                      ``
