b61f483a199fdb27de38799b84d571583016f406
thin <thin@herenow.io>
2016-10-18 17:44:04 +0800

    check postal code in place

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -145,16 +145,17 @@ class Topic < ActiveRecord::Base
       place = Place.find(self.start_place_id)
 
       rt_user = Topic.current
-      fav = UserFavLocation.where(user_id: rt_user.id, place_id: place.id).take
+      if rt_user.present?
+        fav = UserFavLocation.where(user_id: rt_user.id, place_id: place.id).take
+      end
 
       if fav.present?
         place_name = fav.name
       elsif place.short_name.present? && place.source != Place::HERENOW.to_s
         if place.name.length > 6
           p = place.name.last(6).to_s
-          p.count("0-9") == 6 ? place_name = place.name : place_name = place.name
+          p.count("0-9") == 6 ? place_name = place.short_name : place_name = place.name
         end
-         place_name = place.short_name
       else
         place_name = place.name
       end
@@ -182,9 +183,8 @@ class Topic < ActiveRecord::Base
       elsif place.short_name.present? && place.source != Place::HERENOW.to_s
         if place.name.length > 6
           p = place.name.last(6).to_s
-          p.count("0-9") == 6 ? place_name = place.name : place_name = place.name
+          p.count("0-9") == 6 ? place_name = place.short_name : place_name = place.name
         end
-        place_name = place.short_name
       else
         place_name = place.name
       end
