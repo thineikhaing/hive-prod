67b42d2a7909b5724450ced70f5c1dddc6b462c8
thin <thin@herenow.io>
2016-10-21 13:57:18 +0800

    set 5 mins in taskschedule

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -43,10 +43,10 @@ scheduler.cron '05 00 * * *' do
 
 end
 
-# scheduler.every 2.minutes do
-#  p "check accident!"
-#  SgAccidentHistory.get_incident_and_breakdown
-# end
+scheduler.every 5.minutes do
+ p "check accident!"
+ SgAccidentHistory.get_incident_and_breakdown
+end
 
 # scheduler.every 2.minutes do
 #   p "UAT remote test!"
