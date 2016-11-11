bd324a73e406fc960ec45f62046eaab29214cb37
thin <thin@herenow.io>
2016-11-11 12:00:16 +0800

    change mail setting to gmail in pro|stg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,16 +91,25 @@ Hive::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = :true
   config.action_mailer.raise_delivery_errors = :true
-
   ActionMailer::Base.smtp_settings = {
-      :address              => "smtp.raydiusapp.com",
+      :address              => "smtp.gmail.com",
       :port                 => "587",
       :domain               => "raydiusapp.com",
       :authentication       => :plain,
-      :user_name            => "info@raydiusapp.com",
-      :password             => "raydiusadm1n" ,
-      :enable_starttls_auto => true
+      :user_name            => "thineikhaing@gmail.com",
+      :password             => "abcABC!@#" ,
+      # :enable_starttls_auto => false
   }
+
+  # ActionMailer::Base.smtp_settings = {
+  #     :address              => "smtp.raydiusapp.com",
+  #     :port                 => "587",
+  #     :domain               => "raydiusapp.com",
+  #     :authentication       => :plain,
+  #     :user_name            => "info@raydiusapp.com",
+  #     :password             => "raydiusadm1n" ,
+  #     :enable_starttls_auto => true
+  # }
 end
 
 # For factual
