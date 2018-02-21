f9365587ed72565657767b3a4e47e1d5ccbd7859
thin <thineikhaing@gmail.com>
2018-02-21 16:02:05 +0800

    set sheduler back for sg incident and breadkdown

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -45,10 +45,10 @@ scheduler.cron '05 00 * * *' do
 
 end
 
-# scheduler.every 5.minutes do
-#  p "check accident!"
-#  SgAccidentHistory.get_incident_and_breakdown
-# end
+scheduler.every 5.minutes do
+ p "check accident!"
+ SgAccidentHistory.get_incident_and_breakdown
+end
 
 
 # command out
