dd08b455015783eea26419983168bf7136fe6885
thin <thineikhaing@gmail.com>
2018-06-26 16:39:52 +0800

    config update

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,10 +18,11 @@ Hive::Application.configure do
   # Cache control for static assets
   #config.static_cache_control = "public, max-age=86400"
 
+  config.serve_static_assets = true
   # Compress JavaScripts and CSS
   config.assets.compress = true
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
