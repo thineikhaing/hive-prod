876ac474b501c737b989b51fed453b7ea07f5757
thin <thineikhaing@gmail.com>
2018-06-26 16:13:48 +0800

    rollback config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,11 +21,10 @@ Hive::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
