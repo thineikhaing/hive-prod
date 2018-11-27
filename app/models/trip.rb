92a1a843d30d9cf0b4ed71226e868bbf3d5f1297
thin <thineikhaing@gmail.com>
2018-11-27 17:20:20 +0800

    add trip depart and arrive place info

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,11 +1,12 @@
 class Trip < ApplicationRecord
 
   belongs_to :user
-  belongs_to :depatures, class_name: "Place", foreign_key: "start_place_id",primary_key: :id
-  belongs_to :arrivals, class_name: "Place", foreign_key: "end_place_id",primary_key: :id
+  belongs_to :depart, class_name: "Place", foreign_key: "start_place_id",primary_key: :id
+  belongs_to :arrive, class_name: "Place", foreign_key: "end_place_id",primary_key: :id
+
 
   # Setup hstore
   store_accessor :data
 
-  
+
 end
